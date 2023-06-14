69ef8583ca80742a2d16450adb69aa52f25d0848
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-14 22:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+var audio;
+
+function playAudio(filename) {
+  stopAudio(); // Stop any currently playing audio
+
+  audio = new Audio('sounds/' + filename);
+  audio.play();
+}
+
+function stopAudio() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+}
