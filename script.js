62248f306585ce3474920f40183829ac3fd45309
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-15 17:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 //your JS code here. If required.
-function playSound(soundName) {
-  var audio = new Audio('sounds/' + soundName + '.mp3');
-  audio.play();
+const buttons = document.querySelectorAll('.btn');
+
+// Function to play the corresponding sound
+function playSound(soundFile) {
+  const sound = new Audio(soundFile);
+  sound.play();
 }
 
-function stopSound() {
-  var audioElements = document.getElementsByTagName('audio');
-  for (var i = 0; i < audioElements.length; i++) {
-    audioElements[i].pause();
-    audioElements[i].currentTime = 0;
-  }
-}
-
+// Event listener for button clicks
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case 'applause':
+        playSound('https://www.computerhope.com/jargon/m/example.mp3');
+        break;
+      case 'boo':
+        playSound('https://www.computerhope.com/jargon/m/example.mp3');
+        break;
+      case 'gasp':
+        playSound('https://www.computerhope.com/jargon/m/example.mp3');
+        break;
+      case 'toda':
+        playSound('https://www.computerhope.com/jargon/m/example.mp3');
+        break;
+      case 'victory':
+        playSound('https://www.computerhope.com/jargon/m/example.mp3');
+        break;
+      case 'wrong':
+        playSound('https://www.computerhope.com/jargon/m/example.mp3');
+        break;
+      case 'stop':
+        pauseSound('https://www.computerhope.com/jargon/m/example.mp3');
+        break;
+      default:
+        console.log('Invalid button');
+    }
+  });
+});
