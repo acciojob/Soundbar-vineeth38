3b8dc3d7f5b30539d8205e41762e6fa371b8ebca
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-15 17:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your JS code here. If required.
-var x = document.getElementById("myAudio"); 
+function playSound(soundName) {
+  var audio = new Audio('sounds/' + soundName + '.mp3');
+  audio.play();
+}
 
-function playAudio() { 
-  x.play(); 
-} 
+function stopSound() {
+  var audioElements = document.getElementsByTagName('audio');
+  for (var i = 0; i < audioElements.length; i++) {
+    audioElements[i].pause();
+    audioElements[i].currentTime = 0;
+  }
+}
 
-function stopAudio() { 
-  x.pause(); 
-} 
